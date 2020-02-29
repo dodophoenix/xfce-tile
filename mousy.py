889fd01d5ea2ee0acdd4bba33d9a9b4a0a4c9c17
Benjamin Jacob <mail@benjaminjacob.dev>
2020-02-29 16:38:54 +0100

    keep win in forground place mouse over last moved window (avoid focus loss for some configurations) discover tiling direction using pointer (mousy.py)

--- mousy.py
+++ mousy.py
@@ -1,4 +1,14 @@
-# Python 2/3 compatibility.
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright 2020 B.Jacob
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 from __future__ import print_function
 #
 # reads a direction from touchpad/pointer and invokes pywin.py to tile the window
@@ -199,7 +209,6 @@ def main(argv):
                     log("interrupted")
         finally:
             display.close()
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
